8acd9b958dca355072bee3635a9d7e36887370a8
JJknight64 <jwd8744@rit.edu>
2024-11-21 08:44:42 -0500

    Add project files.

---
+++ ModernWinFormsApp/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ModernWinFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAdd = new Button();
+            txtName = new TextBox();
+            label1 = new Label();
+            lstNames = new ListBox();
+            SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(168, 68);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(94, 29);
+            btnAdd.TabIndex = 0;
+            btnAdd.Text = "Add Name";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += button1_Click;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(168, 35);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(224, 27);
+            txtName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // lstNames
+            // 
+            lstNames.FormattingEnabled = true;
+            lstNames.Location = new Point(12, 35);
+            lstNames.Name = "lstNames";
+            lstNames.Size = new Size(150, 104);
+            lstNames.TabIndex = 3;
+            lstNames.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lstNames);
+            Controls.Add(label1);
+            Controls.Add(txtName);
+            Controls.Add(btnAdd);
+            Name = "Form1";
+            Text = "Names";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnAdd;
+        private TextBox txtName;
+        private Label label1;
+        private ListBox lstNames;
+    }
+}
